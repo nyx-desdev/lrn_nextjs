a4e18e0d8973bf23210731c5ce7f439927ea8d93
nyx-desdev <nyx.desdev@gmail.com>
2024-03-17 17:32:28 +0530

    generate metadata with example

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,10 +1,45 @@
-import type { Metadata } from "next";
+import type { Metadata, ResolvingMetadata } from "next";
 
+type Props = {
+  params: { id: string };
+};
 
-export async function generateMetadata({ params }): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return { title: "users name" };
 }
 
 export default function Team() {
   return <h1>Team</h1>;
 }
+
+/**
+ * Example
+ */
+
+// type Props = {
+//   params: { id: string }
+//   searchParams: { [key: string]: string | string[] | undefined }
+// }
+
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   // read route params
+//   const id = params.id
+
+//   // fetch data
+//   const product = await fetch(`https://.../${id}`).then((res) => res.json())
+
+//   // optionally access and extend (rather than replace) parent metadata
+//   const previousImages = (await parent).openGraph?.images || []
+
+//   return {
+//     title: product.title,
+//     openGraph: {
+//       images: ['/some-specific-page-image.jpg', ...previousImages],
+//     },
+//   }
+// }
+
+// export default function Page({ params, searchParams }: Props) {}
